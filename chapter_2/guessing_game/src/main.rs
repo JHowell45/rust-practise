4370663c1a6c8b2be3734a64a743332fe76b6801
JHowell45 <jhowell45@googlemail.com>
2020-12-21 22:49:00 +0000

    Created base chapter.

--- chapter_2/guessing_game/src/main.rs
+++ chapter_2/guessing_game/src/main.rs
@@ -1,12 +1,24 @@
-use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io::stdin;
+
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(0..101);
+    println!("Secret number: {}", secret_number);
+
     println!("Please input your guess: ");
-
+    
     let mut guess = String::new();
+    stdin().read_line(&mut guess).expect("Failed to read line!");
+    let guess: u32 = guess.trim().parse().expect("Failed to convert to 8 bit int!");
+    println!("You guessed: {}", guess);
 
-    io::stdin().read_line(&mut guess).expect("Failed to read line!");
-
-    print!("You guessed: {}", guess);
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
