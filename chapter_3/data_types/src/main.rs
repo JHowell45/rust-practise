f4a2365c93cd22256aeda28d1c38f9e8210a52e9
Jacob <jacob@notactivelylooking.com>
2020-12-23 18:50:46 +0000

    Working on data types.

--- chapter_3/data_types/src/main.rs
+++ chapter_3/data_types/src/main.rs
@@ -3,4 +3,12 @@ fn main() {
 	println!("x: {}", x);
 	let y: f32 = 3.0; // f32
 	println!("y: {}", y);
+
+	let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+	let (x, y, z) = tup;
+
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+    println!("The value of z is: {}", z);
 }
