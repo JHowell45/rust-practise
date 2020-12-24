8f888b59649a40d5a556ef72e0c5e2c980db9f16
Jacob <jacob@notactivelylooking.com>
2020-12-24 18:12:12 +0000

    finished with data types.

--- chapter_3/data_types/src/main.rs
+++ chapter_3/data_types/src/main.rs
@@ -10,5 +10,23 @@ fn main() {
 
     println!("The value of x is: {}", x);
     println!("The value of y is: {}", y);
-    println!("The value of z is: {}", z);
+	println!("The value of z is: {}", z);
+
+	let a = [1, 2, 3, 4, 5];
+
+    let first = a[0];
+	let second = a[1];
+	
+    println!("The value of first is: {}", first);
+	println!("The value of second is: {}", second);
+	
+	let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let a = x.0;
+    let b = x.1;
+	let c = x.2;
+	
+	println!("The value of a is: {}", a);
+	println!("The value of b is: {}", b);
+	println!("The value of c is: {}", c);
 }
